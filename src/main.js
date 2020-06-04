ce23b8fefe4718204bc96d6f1686de33d9e5d1bc
yangming <472102644@qq.com>
2020-06-04 17:25:04 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import deps from './deps.json'
 
 Vue.config.productionTip = false
 xm.sourceCheck && xm.sourceCheck({
   // 卡片管理后台登录后，token 从 chrome -> application -> cookie 中查取
   token: '',
-  ids: [],
+  ids: deps.ids,
   isLocal: process.env.NODE_ENV === 'development'
 })
 
