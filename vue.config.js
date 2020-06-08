3b6248ae830b8c2d51af6243fdf14cc02af43a42
yangming <472102644@qq.com>
2020-06-08 14:44:46 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
+const path = require('path')
+const isLocal = process.env.NODE_ENV === 'development'
+
 module.exports = {
   configureWebpack: {
+  },
+  chainWebpack (config) {
+    config
+      .plugin('html')
+      .tap(args => {
+        return isLocal ? [{ template: path.resolve(__dirname, '../public/index-local.html') }] : args
+      })
   }
 }
