15f9ffbca98a9362e364d9c883516587513ca8ad
yangming <472102644@qq.com>
2020-06-17 12:13:29 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -2,15 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import deps from './card-deps.json'
 import 'tcon'
 
 Vue.config.productionTip = false
-
+const local = process.env.NODE_ENV === 'development'
+if (local) {
+  window.xm.sourceCheck({
+    // 使用的卡片组件名称，从管理后台上可以查到
+    componentNames: deps
+  })
+}
 // 每次 page show 去做检查更新
 window.xm.on('onPageShow', () => {
   xm.sourceCheck({
     // 使用的卡片组件名称，从管理后台上可以查到
-    componentNames: []
+    componentNames: deps
   })
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 const path = require('path')
-const isLocal = process.env.NODE_ENV === 'development'
+// const isLocal = process.env.NODE_ENV === 'development'
 
 module.exports = {
   publicPath: './',
-  chainWebpack (config) {
-    config
-      .plugin('html')
-      .tap(args => {
-        return isLocal ? [{ template: path.resolve(__dirname, './public/index-local.html') }] : args
-      })
-  },
+  // chainWebpack (config) {
+  //   config
+  //     .plugin('html')
+  //     .tap(args => {
+  //       return isLocal ? [{ template: path.resolve(__dirname, './public/index-local.html') }] : args
+  //     })
+  // },
   configureWebpack: {
     externals: { vue: 'Vue' }
   }
