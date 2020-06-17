ef6473caceac637eac5004aa3ad8151d59f3a822
yangming <472102644@qq.com>
2020-06-17 14:32:21 +0800

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,25 @@
   <div class="home">
     <p>Hello World!</p>
     <p>讯盟小程序开发</p>
+
+    <xmcard-render
+      v-for="name in deps"
+      :key="name"
+      :name="name"
+    />
   </div>
 </template>
 
 <script>
+import deps from '../card-deps.json'
+
 export default {
   name: 'Home',
+  data () {
+    return {
+      deps
+    }
+  },
   components: {
   }
 }
